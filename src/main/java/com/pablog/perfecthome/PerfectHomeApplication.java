80113d120132dc2d2921ff6c9724ce58d1c5b6f5
PabloG02 <devpablogg@gmail.com>
2024-11-02 18:19:04 +0100

    Initial commit

---
+++ src/main/java/com/pablog/perfecthome/PerfectHomeApplication.java
@@ -0,0 +1,13 @@
+package com.pablog.perfecthome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablog/perfecthome/PerfectHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablog.perfecthome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
